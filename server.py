09b7fd2243dd6ad0d5ba1dde3d783e9bbb77a0ea
Trevor Cash <tjcashit@gmail.com>
2022-04-10 02:36:12 -0500

    Project complete

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from operator import methodcaller
 from turtle import clear
 from flask import Flask, render_template, session, redirect, request
 app = Flask(__name__)
@@ -9,11 +10,25 @@ def counter():
         session["counter"] += 1
     else:
         session["counter"] = 1
+    if "visits" in session:
+        session["visits"] += 1
+    else:
+        session["visits"] = 1
     return render_template("index.html", session = session)
 
 @app.route('/destroy_session')
 def destroy_session():
     session.clear()
+    return redirect('/')
+
+@app.route('/by_2')
+def by_2():
+    session["counter"] += 1
+    return redirect('/')
+
+@app.route('/this_to_counter', methods = ['POST'])
+def this_to_counter():
+    session["counter"] += (int(request.form["more_to_counter"]) - 1)
     return redirect('/')
 
 if __name__ == "__main__":
